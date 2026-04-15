8575e2b0933fd1529d5e08f732d4058c15ed3d2b
Gabrovez <gabrovez321@gmail.com>
2026-04-15 10:31:51 +0300

    Add Roll Dice button

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -10,6 +10,8 @@ class GradientContainer extends StatelessWidget {
   final Color color1;
   final Color color2;
   final Color color3;
+
+  void rollDice() {}
   @override
   Widget build(BuildContext context) {
     return  Container(
@@ -25,9 +27,17 @@ class GradientContainer extends StatelessWidget {
             ),
           ),
           child: Center(
-            child: Image.asset(
-              'assets/images/dice-1.png',
-              width: 300,
+            child: Column(
+              children: [
+                Image.asset(
+                  'assets/images/dice-1.png',
+                  width: 300,
+                ),
+                TextButton(
+                    onPressed: rollDice,
+                     child: Text("Roll Dice"),
+                ),
+              ],
             ),
           ),
         );
